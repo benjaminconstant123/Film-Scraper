547967ded6522b8da512f050ab14e1eb293ab989
benjaminconstant123 <152168776+benjaminconstant123@users.noreply.github.com>
2024-06-08 18:12:12 +0200

    fix bug double img

--- scraper.py
+++ scraper.py
@@ -1,27 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL de la page
-url = "https://www.themoviedb.org/movie"
+# URL de la page à scraper sur TMDb
+url = 'https://www.themoviedb.org/movie'
 
-# Envoi d'une requete HTTP
-reponse = requests.get(url)
+# Envoyer une requête HTTP à l'URL
+response = requests.get(url)
 
-if reponse.status_code == 200:
-    print('requete good')
-
-    soup = BeautifulSoup(reponse.text, 'html.parser')
-
+# Vérifier que la requête a réussi
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
     # Trouver les éléments contenant les titres de films et les affiches
     movies = soup.select('.card.style_1 .content')
-    posters = soup.select('.card.style_1 .image')
+    posters = soup.select('.card.style_1 .image img')
     
     for index, movie in enumerate(movies):
         title = movie.find('h2').text.strip()
-        poster_url = posters[index].find('img')['src']
+        poster_url = posters[index]['src']
         print(f'Titre: {title}')
         print(f'Affiche: {poster_url}')
-
 else:
-    print('requete fail')
-    print('Code d\'état HTTP:', reponse.status_code)
+    print('La requête a échoué')
